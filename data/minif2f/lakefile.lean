27985b510905db5f9a7caaf0475c13320612d4d1
mjcs27 <mjcs27@cs.utexas.edu>
2025-02-03 22:51:37 +0000

    update Lean version to v4.15.0

--- data/minif2f/lakefile.lean
+++ data/minif2f/lakefile.lean
@@ -11,4 +11,4 @@ lean_lib «Minif2f» where
 lean_exe «minif2f» where
   root := `Main
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.14.0"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.15.0"
---
+++ data/minif2f/temptodel173862065067696475374215.lean
@@ -0,0 +1,25 @@
+import Mathlibimport Mathlibimport Mathlib
+
+theorem aime_1983_p1
+  (x y z w : ℝ)
+  (hx : 1 < x)
+  (hy : 1 < y)
+  (hz : 1 < z)
+  (hw : 0 < w)
+  (h₁ : Real.logb x w = 24)
+  (h₂ : Real.logb y w = 40)
+  (h₃ : Real.logb (x * y * z) w = 12) :
+  Real.logb z w = 60  :=  by
+
+
+
+theorem aime_1983_p1
+  (x y z w : ℝ)
+  (hx : 1 < x)
+  (hy : 1 < y)
+  (hz : 1 < z)
+  (hw : 0 < w)
+  (h₁ : Real.logb x w = 24)
+  (h₂ : Real.logb y w = 40)
+  (h₃ : Real.logb (x * y * z) w = 12) :
+  Real.logb z w = 60  :=  by
